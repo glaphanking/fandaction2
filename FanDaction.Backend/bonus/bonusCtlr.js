d840701b0cad7c83748e078e1bf68128117bebe4
Kristopher Sams <glaphanking@gmail.com>
2016-04-08 08:41:24 -0400

    Backend creation

---
+++ FanDaction.Backend/bonus/bonusCtlr.js
@@ -0,0 +1,36 @@
+﻿function bonusCtlr($scope, $animate, dataservice) {
+    vm = this;
+
+    vm.bonuses = [];
+
+    loadBonuses();
+
+    function loadBonuses() {
+
+    }
+
+    function change(action, parameter, enable, condition) {
+        var inputsArray = new Array();
+        if(condition == 'true')
+        {
+            if(confirm('Are you sure you want to ' + action + ' this Bonus? '))
+            {
+                if(action == 'update')
+                {
+
+                }
+
+                if(action == 'add')
+                {
+                    inputsArray = CollectBonusFormData('bonusAddInFormationForm');
+                    for(var i=0; i < inputsArray.length; i++)
+                    {
+                        inputName = inputsArray[i].name;
+                        inputValue = inputsArray[i].value;
+
+                    }
+                }
+            }
+        }
+    }
+}
